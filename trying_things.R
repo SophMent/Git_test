477a11f5964f11655d79425834bc444a7924fc2d
SophMent <som@niva.no>
2021-09-27 11:24:56 +0200

    added plot

--- trying_things.R
+++ trying_things.R
@@ -6,3 +6,10 @@ library(ggplot2)
 
 ggplot(data, mapping = aes(Station_name , Conc, colour = Station_name)) +
   geom_point()
+
+
+ggplot(data, mapping = aes(Substance , Conc, colour = Substance)) +
+  geom_point()
+
+ggplot(data, mapping = aes(Substance , Conc, shape = Substance)) +
+  geom_point()
